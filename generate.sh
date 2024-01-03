a6d6123e92258d3265a1db3f16c88f533f5ce650
qwerty01 <qw3rty01@gmail.com>
2024-01-03 12:20:16 -0500

    TheTVDB initial generated project

---
+++ generate.sh
@@ -0,0 +1,6 @@
+!/bin/bash
+wget https://thetvdb.github.io/v4-api/swagger.yml
+openapi-generator generate -g rust -i swagger.yml --skip-validate-spec
+rm swagger.yml
+sed -i 's/version = "\([0-9]\+\)\.\([0-9]\+\)"/version = "\1.\2.0"/' Cargo.toml
+cargo fmt
